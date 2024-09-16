d4b08233aa15fbef5e9d449cabb9f5b2e513a480
Danielfgb <dgomezbernal24@gmail.com>
2024-09-16 16:37:13 -0500

    corrección: desplegable de placas

--- app/screens/py/calibrar.py
+++ app/screens/py/calibrar.py
@@ -1,21 +1,20 @@
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import Screen
 from kivy.clock import Clock
 from kivy.properties import StringProperty, ListProperty
-from kivy.uix.dropdown import DropDown
-from kivy.uix.button import Button
+from kivymd.uix.menu import MDDropdownMenu
+from kivy.metrics import dp
 
 import json
 import shutil
 
 from tools.camara import CameraController
-from tools.cnc import CNCController  
+from tools.cnc import CNCController
 from tools.GPIO import GPIOController
 
 class CalibrarScreen(Screen):
     current_time = StringProperty()
     camera_controller = None
     gpio_controller = None
-    dropdown = None
     selected_tag_location = ListProperty([0.0, 0.0, 0.0])  # Coordenadas del tag seleccionado
     new_location = ListProperty([0.0, 0.0, 0.0])  # Nuevas coordenadas para el movimiento manual
 
@@ -34,8 +33,7 @@ class CalibrarScreen(Screen):
         self.camera_controller = CameraController(camera_id=0)
         self.camera_controller.start_camera(self.ids.camera_image)
 
-        # Configura el menú desplegable
-        self.dropdown = DropDown()
+        # Cargar los datos del JSON y configurar el menú
         self.data = {}  # Variable para cargar los datos del JSON
         self.load_dropdown_items()
 
@@ -65,46 +63,90 @@ class CalibrarScreen(Screen):
         self.manager.current = 'inicio'
         self.gpio_controller.deactivate_cnc()
 
-    def show_dropdown(self):
-        """Abre el dropdown cuando se hace clic en el botón."""
-        self.dropdown.open(self.ids.dropdown_button)
-
-    def create_dropdown_items(self, items):
-        """Crea las opciones del dropdown con los elementos del JSON."""
-        self.dropdown.clear_widgets()
-        for item in items:
-            btn = Button(text=str(item), size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: self.select_dropdown_item(btn.text))
-            self.dropdown.add_widget(btn)
+    def show_menu(self, caller):
+        """Abre el menú desplegable con los números de tags."""
+        menu_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": f"Placa {tag['tag']}",
+                "height": dp(56),
+                "on_release": lambda x=f"Placa {tag['tag']}": self.select_dropdown_item(x)
+            } for tag in self.data.get('tags', [])
+        ]
+        
+        self.menu = MDDropdownMenu(
+            caller=caller,
+            items=menu_items,
+            width_mult=3,
+            max_height =dp(336)
+        )
+        self.menu.open()
 
     def select_dropdown_item(self, value):
-        """Acción que se realiza al seleccionar un ítem del dropdown."""
-        print(f"Selección del dropdown: {value}")
+        """Acción que se realiza al seleccionar un ítem del menú."""
+        tag_number = value.split(" ")[1]  # Obtener solo el número del tag
         self.ids.dropdown_button.text = value
-        self.dropdown.dismiss()
+        self.menu.dismiss()
 
         # Busca la ubicación en el JSON
-        tag_data = next((tag for tag in self.data['tags'] if str(tag['tag']) == value), None)
-        if tag_data:
-            location = tag_data.get('location', [0.0, 0.0, 0.0])
+        tag_data = next((tag for tag in self.data['tags'] if str(tag['tag']) == tag_number), None)
+        if tag_data and 'location' in tag_data:
+            location = tag_data['location']
             self.selected_tag_location = location
             self.new_location = location.copy()
             print(f"Ubicación seleccionada: {self.selected_tag_location}")
-
-            # Mover a la posición del tag seleccionado
             self.move_to_tag()
+        else:
+            print(f"No se encontró la ubicación para el tag: {tag_number}")
 
     def load_dropdown_items(self):
         """Carga los ítems del dropdown desde un archivo JSON."""
         try:
             with open('./app/config/coordinates_10.json', 'r') as file:
                 self.data = json.load(file)
-                tags = [str(tag['tag']) for tag in self.data.get('tags', [])]
-                self.create_dropdown_items(tags)
         except FileNotFoundError:
             print("Archivo JSON no encontrado.")
         except json.JSONDecodeError:
             print("Error al decodificar el archivo JSON.")
+
+    def move_to_tag(self):
+        """Envía las coordenadas del tag seleccionado al CNC para que se mueva a dicha posición."""
+        if self.selected_tag_location:
+            x, y, z = self.selected_tag_location
+            self.cnc.move_to(x=x, y=y, z=0)  # Mover siempre a Z=0
+            self.new_location = [x, y, 0]
+            print(f"Moviendo a la posición del tag: X={x}, Y={y}, Z=0")
+        else:
+            print("No se ha seleccionado un tag o las coordenadas están incompletas.")
+
+    def save_settings(self):
+        print(f"Guardando la nueva ubicación: {self.new_location}")
+
+        # Busca el tag seleccionado en los datos
+        for tag in self.data['tags']:
+            if str(tag['tag']) == self.ids.dropdown_button.text.split(" ")[1]:
+                tag['location'] = self.new_location
+                break
+
+        # Guarda los datos actualizados en el archivo JSON
+        with open('./app/config/coordinates_10.json', 'w') as file:
+            json.dump(self.data, file, indent=4)
+        
+        print("Configuración guardada.")
+
+    def go_home(self):
+        self.cnc.go_home()
+        self.new_location = [0.0, 0.0, 0.0]
+
+    def reset_coordinates(self):
+        """Restablece las coordenadas copiando el contenido del archivo JSON backup."""
+        backup_file = './app/config/coordinates_10 copy.json'
+        target_file = './app/config/coordinates_10.json'
+        try:
+            shutil.copyfile(backup_file, target_file)
+            print(f"Coordenadas restablecidas desde {backup_file}")
+        except Exception as e:
+            print(f"Error al restablecer coordenadas: {e}")
 
     # Métodos para mover los ejes manualmente
     def move_x_positive(self):
@@ -154,44 +196,3 @@ class CalibrarScreen(Screen):
             self.new_location[2] = self.MIN_Z
         self.cnc.move_to(z=self.new_location[2])
         print(f"Moviendo eje Z negativo a: {self.new_location[2]}")
-
-    def move_to_tag(self):
-        """Envía las coordenadas del tag seleccionado al CNC para que se mueva a dicha posición."""
-        if self.selected_tag_location:
-            x, y, z = self.selected_tag_location
-            self.cnc.move_to(x=x, y=y, z=0)
-            self.new_location = [x, y, 0]
-            print(f"Moviendo a la posición del tag: X={x}, Y={y}, Z={0}")
-        else:
-            print("No se ha seleccionado un tag o las coordenadas están incompletas.")
-
-    def save_settings(self):
-        print(f"Guardando la nueva ubicación: {self.new_location}")
-
-        # Busca el tag seleccionado en los datos
-        for tag in self.data['tags']:
-            if str(tag['tag']) == self.ids.dropdown_button.text:
-                tag['location'] = self.new_location
-                break
-
-        # Guarda los datos actualizados en el archivo JSON
-        with open('./app/config/coordinates_10.json', 'w') as file:
-            json.dump(self.data, file, indent=4)
-        
-        print("Configuración guardada.")
-
-######################################33
-
-    def go_home(self):
-        self.cnc.go_home()
-        self.new_location = [0.0, 0.0, 0.0]
-
-    def reset_coordinates(self):
-        """Restablece las coordenadas copiando el contenido del archivo JSON backup."""
-        backup_file = './app/config/coordinates_10 copy.json'
-        target_file = './app/config/coordinates_10.json'
-        try:
-            shutil.copyfile(backup_file, target_file)
-            print(f"Coordenadas restablecidas desde {backup_file}")
-        except Exception as e:
-            print(f"Error al restablecer coordenadas: {e}")
